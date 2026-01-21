39cddeaed8b4a8f4819e1c7c5366e93555ba9550
Sujal Debta <sujaldebta172@gmail.com>
2026-01-21 16:11:58 +0530

    Update settings.py

--- EmployeeRecordHub/settings.py
+++ EmployeeRecordHub/settings.py
@@ -120,8 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
